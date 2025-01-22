9ec10f2c4bb6319aba6af613be1fc2208fbeb313
Shavkunov Pavel <shpavser@gmail.com>
2025-01-23 00:30:19 +0500

    User https keys

--- src/opachat/main.go
+++ src/opachat/main.go
@@ -21,17 +21,17 @@ var ct = map[string]int64{
 func main() {
 	e := tools.Env(false)
 
-	if e.Debug {
-		startDevTLS(e)
+	if e.Acme {
+		startAcme(e)
 		return
 	}
 
-	startTLS(e)
+	startSelf(e)
 }
 
-func startDevTLS(e *tools.Configuration) {
-	fmt.Printf("\n[%s] %s started\ncrt\t\t%s\nkey\t\t%s\naddress\t\t%s:%d\ncsrf\t\t%s\n",
-		"debug", e.Appname,
+func startSelf(e *tools.Configuration) {
+	fmt.Printf("\n[%s] %s\ncrt\t\t%s\nkey\t\t%s\naddress\t\t%s:%d\ncsrf\t\t%s\n",
+		"self", e.Appname,
 		e.Crt, e.Key,
 		e.Address, e.Port,
 		tools.GetKeyCSRF(),
@@ -44,15 +44,16 @@ func startDevTLS(e *tools.Configuration) {
 		Handler:        mux,
 		ReadTimeout:    time.Duration(ct["ReadTimeout"] * int64(time.Second)),
 		WriteTimeout:   time.Duration(ct["WriteTimeout"] * int64(time.Second)),
+		IdleTimeout:    time.Duration(ct["IdleTimeout"] * int64(time.Second)),
 		MaxHeaderBytes: 1 << 20,
 	}
 
 	log.Fatalln(server.ListenAndServeTLS(e.Crt, e.Key))
 }
 
-func startTLS(e *tools.Configuration) {
-	fmt.Printf("\n[%s] %s started\nacmehost\t%s\ndirCache\t%s\naddress\t\t%s:%d\ncsrf\t\t%s\n",
-		"prod", e.Appname,
+func startAcme(e *tools.Configuration) {
+	fmt.Printf("\n[%s] %s\nacmehost\t%s\ndirCache\t%s\naddress\t\t%s:%d\ncsrf\t\t%s\n",
+		"acme", e.Appname,
 		e.Acmehost, e.DirCache, e.Address, e.Port,
 		tools.GetKeyCSRF(),
 	)
@@ -66,7 +67,7 @@ func startTLS(e *tools.Configuration) {
 	mux := controllers.GetRouters()
 
 	server := &http.Server{
-		Addr:           fmt.Sprintf(":%d", e.Port),
+		Addr:           fmt.Sprintf("%s:%d", e.Address, e.Port),
 		Handler:        mux,
 		ReadTimeout:    time.Duration(ct["ReadTimeout"] * int64(time.Second)),
 		WriteTimeout:   time.Duration(ct["WriteTimeout"] * int64(time.Second)),
