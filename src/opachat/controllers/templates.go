16ea6e54414b3f5fc3afa22a3631aedef7f546a3
Shavkunov Pavel <shpavser@gmail.com>
2025-05-23 12:21:49 +0500

    Remove jquery start

--- src/opachat/controllers/templates.go
+++ src/opachat/controllers/templates.go
@@ -10,15 +10,15 @@ func getSiteTemplates(filenames []string) (tmpl *template.Template) {
 	var files []string
 
 	for _, file := range filenames {
-		files = append(files, fmt.Sprintf("templates/site/%s.html", file))
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
 	}
 
 	tmpl = template.Must(template.New("").ParseFiles(files...))
 	return
 }
 
-func GenerateHTMLEmp(w http.ResponseWriter, r *http.Request, data interface{}, filenames []string) {
-	filenames = append(filenames, "layout_emp")
+func GenerateHTMLEmp(w http.ResponseWriter, r *http.Request, data interface{}, filenames ...string) {
+	filenames = append(filenames, "lays/layout")
 
-	getSiteTemplates(filenames).ExecuteTemplate(w, "layout_emp", data)
+	getSiteTemplates(filenames).ExecuteTemplate(w, "layout", data)
 }
